96175e99ad21322fe0d563a8de209a6e3993b587
1Clavero <arturoclavero01@gmail.com>
2024-05-16 22:18:35 +0700

    modify light 1

--- src/3_ray/light.c
+++ src/3_ray/light.c
@@ -6,7 +6,7 @@
 /*   By: arturo <arturo@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/16 12:04:22 by arturo            #+#    #+#             */
-/*   Updated: 2024/05/16 22:09:36 by arturo           ###   ########.fr       */
+/*   Updated: 2024/05/16 22:17:22 by arturo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,57 +39,30 @@ void	create_sphere(t_obj *sph)
 	create_vector(&sph->color, 1, 1, 1);
 }
 
-void	calc_light_dir(t_light *light, t_ray ray)
+void	calc_light_dir(t_light *light)
 {
-	(void)ray;
-	//create_tupple(&light->point, 0, 0, 0);
-	//create_vector(&light->dir, 0, 0, 20);
 	substract(light->og, light->point, &light->dir);
 	normalize(light->dir, &light->dir);
 }
 
-void	calc_light_normal(t_light *light, t_obj sph)
-{
-	(void)sph;
-	//create_vector(&light->normal, 0, 0, 1);
-	substract(light->point, sph.og, &light->normal);
-	normalize(light->normal, &light->normal);
-}
-
-void	calc_light_eye(t_light *light, t_ray ray)
-{
-	(void)ray;
-	//create_vec(&light->eye, 0, 0, 1);
-	negate(ray.dir, &light->eye);
-}
-
 void	calc_light_reflection(t_vec in, t_vec normal, t_vec *result)
 {
-	(void)in;
-	(void)normal;
 	t_vec	temp;
 
-	print_t_vec(in);
-	print_t_vec(normal);
-	printf("dot_product ref: %f\n", dot_product(in, normal));
 	scalar_mult(normal, (2 * dot_product(in, normal)), &temp);
-	printf("temp: \n");
-	print_t_vec(temp);
 	substract(temp, in, result);
 	clear_negative_zeros_t_vec(result);
 	normalize(*result, &temp);
 	copy_t_vec(result, temp);
-	printf("reflection: \n");
-	print_t_vec(*result);
-	printf("\n");
-	return ;
 }
 
 void	calc_light_vectors(t_light *light, t_ray ray, t_obj obj)
 {
 	copy_t_vec(&light->point, ray.target);
-	calc_light_normal(light, obj);
-	calc_light_eye(light, ray);
+	substract(light->point, obj.og, &light->normal);
+	normalize(light->normal, &light->normal);
+	negate(ray.dir, &light->eye);
+	//normalize eye?
 }
 
 void	compute_final_color(t_light light, t_obj obj, t_ray *ray)
@@ -97,14 +70,10 @@ void	compute_final_color(t_light light, t_obj obj, t_ray *ray)
 	t_vec	base_color;
 	t_mtrx	color;
 	float	dot;
-	//t_vec	temp;
 
 	blend_colors(light.color, obj.color, &base_color);
-	calc_light_dir(&light, *ray);
+	calc_light_dir(&light);
 	scalar_mult(base_color, light.ambient, &color[AMBIENT]);
-	printf("ambient: \n");
-	print_t_vec(color[AMBIENT]);
-	printf("\n");
 	dot = dot_product(light.dir, light.normal);
 	if (dot < 0)
 	{
@@ -114,29 +83,16 @@ void	compute_final_color(t_light light, t_obj obj, t_ray *ray)
 	else
 	{
 		scalar_mult(base_color, (light.diffuse * dot), &color[DIFFUSE]);
-		printf("diffuse: \n");
-		print_t_vec(color[DIFFUSE]);
-		printf("\n");
 		calc_light_reflection(light.dir, light.normal, &light.reflect);
 		dot = dot_product(light.reflect, light.eye);
 		if (dot <= 0)
 			create_vector(&color[SPECULAR], 0, 0, 0);
 		else
-		{
-			printf("dot: %f\n", dot);
-			printf("pow: %f\n", pow(dot, light.shine));
 			scalar_mult(light.color, (pow(dot, light.shine) \
 			* light.specular), &color[SPECULAR]);
-			printf("specular: \n");
-			print_t_vec(color[SPECULAR]);
-			printf("\n");
-		}
 	}
-	print_matrix(color, 4);
 	add(color[AMBIENT], color[DIFFUSE], &ray->color);
 	add(ray->color, color[SPECULAR], &ray->color);
-	printf("final color:\n");
-	print_t_vec(ray->color);
 }
 
 int	main(void)
@@ -161,4 +117,5 @@ int	main(void)
 	create_vector(&light.color, 1, 1, 1);
 	//END TESTING
 	compute_final_color(light, sph, &ray);
+	print_t_vec(ray.color);
 }
