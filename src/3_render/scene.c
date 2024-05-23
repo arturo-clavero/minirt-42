0bf4626e26d1be76191f7af2322a28d15090ad1f
1Clavero <arturoclavero01@gmail.com>
2024-05-23 11:33:33 +0700

    some acne in some scenes, doubled EPSILON

--- src/3_render/scene.c
+++ src/3_render/scene.c
@@ -6,7 +6,7 @@
 /*   By: arturo <arturo@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/17 18:53:55 by arturo            #+#    #+#             */
-/*   Updated: 2024/05/23 11:14:08 by arturo           ###   ########.fr       */
+/*   Updated: 2024/05/23 11:32:57 by arturo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -92,7 +92,7 @@ void	is_point_in_shadow(t_light *light, t_mlx *mlx)
 	substract(light->og, light->point, &temp);
 	light_to_point_dist = sqrtf(dot_product(temp, temp));
 	normalize(temp, &shadow_ray.dir);
-	scalar_mult(light->normal, EPSILON, &temp);
+	scalar_mult(light->normal, EPSILON * 2, &temp);
 	add(light->point, temp, &shadow_ray.og);
 	if (find_intersection(&shadow_ray, mlx) == TRUE \
 	&& shadow_ray.closest->dist < light_to_point_dist)
