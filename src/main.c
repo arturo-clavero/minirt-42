83695995743f78b2aa110744f0504ae862691c2e
Arturo Clavero <artclave@z1t3c3.42bangkok.com>
2024-07-21 20:54:40 +0700

    esc added

--- src/main.c
+++ src/main.c
@@ -3,14 +3,23 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: arturo <arturo@student.42.fr>              +#+  +:+       +#+        */
+/*   By: artclave <artclave@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/08 16:36:04 by arturo            #+#    #+#             */
-/*   Updated: 2024/05/31 08:38:31 by arturo           ###   ########.fr       */
+/*   Updated: 2024/07/21 20:54:17 by artclave         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "header.h"
+
+int	key_press_hook(int keycode, t_mlx *mlx)
+{
+	if (keycode == 65307) {
+		clean(mlx);
+		exit(0);
+	}
+	return (0);
+}
 
 int	main(void)
 {
@@ -23,6 +32,7 @@ int	main(void)
 	lexer(&mlx, pars);
 	init_scene(&mlx);
 	mlx_put_image_to_window(mlx.mlx, mlx.win, mlx.image.img, 0, 0);
+	mlx_hook(mlx.win, 2, 1, key_press_hook, &mlx);
 	mlx_loop(mlx.mlx);
 	clean(&mlx);
 	return (0);
